3947d9f8e10dbe6f777d50350310034305fb71ff
Teteoo <mateusdev13@gmail.com>
2025-10-31 09:27:09 -0300

    correção erros do js

--- js/posts.js
+++ js/posts.js
@@ -1,28 +1,21 @@
-<<<<<<< HEAD
-btnPostar.onClick = postar;
 
-function postar(){
-=======
-/**
- * Associa o clique do botão à execução da função "postar()"
- */
 btnPostar.onclick = postar;
 
 function postar(){
     // Cria elementos HTML e os armazena em variáveis
->>>>>>> 5629f7bbf0e824ffbe8bc9c2b8ea86aaf7771f92
+
     let post = document.createElement("article");
     let title = document.createElement("h3");
     let text = document.createElement("p");
 
-<<<<<<< HEAD
+
     title.innerText = new Date();
     text.innerText = msg.value;
     msg.value = "";
 
     post.appendChild(title);
     post.appendChild(text);
-=======
+
     /**
      * Define o título da postagem
      * - Uma vez que não temos banco de dados para recuperar o nome de usuário logado
@@ -37,6 +30,5 @@ function postar(){
     post.appendChild(text);
 
     // Aninha o <article> que criamos à section#postagens
->>>>>>> 5629f7bbf0e824ffbe8bc9c2b8ea86aaf7771f92
     postagens.appendChild(post);
 }
